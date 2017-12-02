d04d1c1ea27ca964076184fc9cb4bc5709396edd
cakeinpanic <cakeinpanic@gmail.com>
2017-12-02 14:12:03 +0300

    solve second puzzle extended

--- puzzles.js
+++ puzzles.js
@@ -19,3 +19,20 @@ function second(input) {
         .reduce((a, b) => a + b)
 }
 
+function secondExtended(input) {
+    function getLineDivided(line, index) {
+        return line.map((number, i) => i === index ? 0 : number / line[index])
+                   .filter(dividedNumber => !!dividedNumber && Math.ceil(dividedNumber) === Math.floor(dividedNumber));
+    }
+
+    return input
+        .map(line => {
+            let i = 0;
+            let dividedLine = getLineDivided(line, i);
+            while (!dividedLine.length) {
+                dividedLine = getLineDivided(line, ++i);
+            }
+            return dividedLine[0];
+        })
+        .reduce((a, b) => a + b)
+}
