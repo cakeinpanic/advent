0e9634601df05300a5b9320540637320070d00da
cakeinpanic <cakeinpanic@gmail.com>
2017-12-02 14:01:37 +0300

    solve first puzzle extended

--- puzzles.js
+++ puzzles.js
@@ -1,8 +1,8 @@
-function first(input) {
+function first(input, step = 1) {
     input = input.split('').map(digit => +digit);
 
     let subResult = input
-        .filter((digit, i) => input[(i + 1) % input.length] === digit);
+        .filter((digit, i) => input[(i + step) % input.length] === digit);
 
     if (subResult[subResult.length - 1] === subResult[0]) {
         subResult.pop();
@@ -13,9 +13,9 @@ function first(input) {
     return subResult.reduce((a, b) => a + b);
 }
 
-
 function second(input) {
     return input
         .map(line => Math.max(...line) - Math.min(...line))
         .reduce((a, b) => a + b)
 }
+
