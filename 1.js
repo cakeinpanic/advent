ff0d4e0d5041d5a06131b54542c9c71672478672
cakeinpanic <cakeinpanic@gmail.com>
2017-12-04 00:53:12 +0300

    add separate file for each day

---
+++ 1.js
@@ -0,0 +1,9 @@
+function first(input, step = 1) {
+    input = input.split('').map(digit => +digit);
+
+    let result = 0;
+    // reduce can be used, but readability would be worse
+    input.forEach((digit, i) => result += input[(i + step) % input.length] === digit ? digit : 0);
+
+    return result;
+}
--- 2.js
+++ 2.js
@@ -1,13 +1,3 @@
-function first(input, step = 1) {
-    input = input.split('').map(digit => +digit);
-
-    let result = 0;
-    // reduce can be used, but readability would be worse
-    input.forEach((digit, i) => result += input[(i + step) % input.length] === digit ? digit : 0);
-
-    return result;
-}
-
 function second(input) {
     return input
         .map(line => Math.max(...line) - Math.min(...line))
