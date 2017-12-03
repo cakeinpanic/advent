01260f0d98654963c06b65ef2f8b133ac7be4e97
cakeinpanic <cakeinpanic@gmail.com>
2017-12-04 00:24:53 +0300

    add third extended puzzle solution

--- third.js
+++ third.js
@@ -1,47 +1,77 @@
-function remainder(t) {
-    return t - Math.floor(t);
+function closestSquareBase(input) {
+    let ceil = Math.pow(Math.ceil(input), 2);
+    let floor = Math.pow(Math.floor(input), 2);
+
+    return ceil - input * input > input * input - floor
+        ? Math.floor(input)
+        : Math.ceil(input)
 }
 
-function closest(t) {
-    return remainder(t) > 0.5 ? Math.ceil(t) : Math.floor(t);
+function getSteps(input) {
+    let sqr = closestSquareBase(Math.sqrt(input));
+    let fullSqr = sqr * sqr;
+    let coeff = 1;
+    let steps = {
+        down: 0, right: 0
+    };
+
+    if (!(sqr % 2)) {
+        coeff = -1;
+        steps.right = 1;
+    }
+
+    steps.right += coeff * Math.floor(sqr / 2);
+    steps.down += coeff * Math.floor(sqr / 2);
+
+    if (input > fullSqr) {
+        steps.right += coeff;
+        steps.down -= coeff * (input - 1 - fullSqr);
+    } else {
+        steps.right -= coeff * (fullSqr - input);
+    }
+
+    return steps;
 }
 
 function third(input) {
-    let sqr = closest(Math.sqrt(input));
-    let fullSqr = sqr * sqr;
-    let steps = {
-        down: 0, up: 0, left: 0, right: 0
-    };
-
-    if (sqr % 2) {
-        steps.down = Math.floor(sqr / 2);
-        steps.right = Math.floor(sqr / 2);
-        if (input > fullSqr) {
-            steps.right++;
-            steps.up = input - 1 - fullSqr;
-        } else {
-            steps.left = fullSqr - input;
-        }
-    } else {
-        steps.left = (sqr / 2);
-        steps.up = sqr / 2 - 1;
-        if (input > fullSqr) {
-            steps.left++;
-            steps.down = input - 1 - fullSqr;
-        } else {
-            steps.right = fullSqr - input;
-        }
-
-    }
-    return Math.abs(steps.left - steps.right) + Math.abs(steps.up - steps.down);
+    let steps = getSteps(input);
+    return Math.abs(steps.right) + Math.abs(steps.down)
 }
 
-third(347991)
+function thirdExtended(input) {
+    let res = [];
+    let n = Math.ceil(Math.pow(input, 1 / 4));
+    let center = Math.ceil(n / 2);
 
-// 37 36  35  34  33  32 31
-// 38 17  16  15  14  13 30
-// 39 18   5   4   3  12 29
-// 40 19   6   1   2  11 28
-// 41 20   7   8   9  10 27
-// 42 21  22  23   24 25 26
-// 43 44  45  46   47 48 49
+    for (let k = 1; k < n * n + 1; k++) {
+        let steps = getSteps(k);
+        let x = center + steps.right - 1;
+        let y = center + steps.down;
+
+        if (!res[y]) {
+            res[y] = [];
+        }
+
+        res[y][x] = k === 1 ? 1 : 0
+            + getElement(y - 1, x)
+            + getElement(y + 1, x)
+            + getElement(y - 1, x + 1)
+            + getElement(y + 1, x + 1)
+            + getElement(y - 1, x - 1)
+            + getElement(y + 1, x - 1)
+            + getElement(y, x - 1)
+            + getElement(y, x + 1);
+
+        if (res[y][x] > input) {
+            return res[y][x];
+        }
+    }
+
+    function getElement(y, x) {
+        return res[y] && res[y][x] || 0;
+    }
+}
+
+console.log(third(347991))
+console.log(thirdExtended(347991))
+console.log(thirdExtended(1000))
