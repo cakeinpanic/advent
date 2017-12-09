f307ed66208ab34576aaad7f68452f7498b57a02
cakeinpanic <cakeinpanic@gmail.com>
2017-12-09 16:17:52 +0300

    add nine extended day solution

--- 9.js
+++ 9.js
@@ -1,42 +1,50 @@
 let input = require('./data/9');
 
-function countSymbols(symbol, input) {
-    let counter = 0;
-    let reg = new RegExp(symbol, 'g')
-    while (reg.test(input)) {
-        counter++
+function removeAndCountTrash(input) {
+    const trashRegexp = /\<([^>]*)\>/;
+    let trashCount = 0;
+    let foundTrash;
+
+    input = input.replace(/\!./g, '');
+    while (foundTrash = trashRegexp.exec(input)) {
+        input = input.replace(foundTrash[0], '');
+        trashCount += foundTrash[1].length;
     }
-    return counter;
+
+    return {
+        cleanInput: input,
+        trashCount
+    };
 }
 
 function nine(input) {
-    const trashRegexp = /\<[^>]*\>/g;
+    const groupRegexp = /\{((\d+ )+)\}/;
 
-    input = input.replace(/\!./g, '')
-                 .replace(/[^\{^\}^\<^\>]/g, '')
-                 .replace(trashRegexp, '')
-                 .replace(/\{\}/g, `1 `)
+    input = removeAndCountTrash(input).cleanInput
+                                      .replace(/[^\{^\}^\<^\>]/g, '')
+                                      .replace(/\{\}/g, `1 `);
 
-    if (countSymbols('<', input) > 0 || countSymbols('>', input) > 0 ||
-        countSymbols('{', input) !== countSymbols('}', input)) {
-        throw new Error('wrong parse');
-    }
 
-    const sumGroupRegexp = /\{((\d+ )+)\}/;
-    while (sumGroupRegexp.test(input)) {
-        let result = sumGroupRegexp.exec(input);
-        let subNumber = result[1].split(' ')
+    while (groupRegexp.test(input)) {
+        let result = groupRegexp.exec(input);
+        let subNumber = result[1].replace(/ $/, '')
+                                 .split(' ')
                                  .reverse()
                                  .map(n => +n + 1);
 
-        // remove last phantom match
-        subNumber.shift();
         subNumber.push(1);
         input = input.replace(result[0], subNumber.join(' ') + ' ')
     }
 
-    return input.split(' ').map(n => +n).reduce((a, b) => +a + b);
+    return input.split(' ')
+                .map(n => +n)
+                .reduce((a, b) => a + b);
 }
 
 
+function nineExtended(input) {
+    return removeAndCountTrash(input).trashCount;
+}
+
 console.log(nine(input));
+console.log(nineExtended(input));
