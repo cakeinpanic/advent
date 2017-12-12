7e047e91200fa255510b6f802bea3a4dc1d8a0e7
cakeinpanic <cakeinpanic@gmail.com>
2017-12-13 01:30:41 +0300

    add eleven day solution

---
+++ 11.js
@@ -0,0 +1,65 @@
+let input = require('./data/11');
+
+function getCount(input, str) {
+    return input.filter(el => el === str).length;
+}
+
+function replaceOpposite(sortedSteps, first, second, replace) {
+    // first should be alphabetially first
+    if (second < first) {
+        return replaceOpposite(sortedSteps, second, first, replace);
+    }
+
+    sortedSteps = [].concat(sortedSteps);
+
+    let firstCount = getCount(sortedSteps, first);
+    let secondCount = getCount(sortedSteps, second);
+
+    let edited = !(firstCount === 0 || secondCount === 0);
+    let addCount = Math.min(firstCount, secondCount);
+
+    sortedSteps.splice(sortedSteps.indexOf(second), addCount);
+    sortedSteps.splice(sortedSteps.indexOf(first), addCount);
+
+    if (!!replace) {
+        sortedSteps = sortedSteps
+            .concat(new Array(addCount).fill(replace))
+            .sort();
+    }
+
+    return {
+        sortedSteps: sortedSteps, edited
+    };
+}
+
+
+function eleven(steps) {
+    steps = steps.sort();
+    let edited = true;
+
+    let groups = [
+        ['nw', 'se'],
+        ['ne', 'sw'],
+        ['n', 's'],
+        ['se', 'sw', 's'],
+        ['n', 'se', 'ne'],
+        ['ne', 'nw', 'n'],
+        ['ne', 's', 'se'],
+        ['nw', 's', 'sw'],
+        ['nw', 'ne', 'n'],
+        ['n', 'sw', 'nw']
+    ];
+
+    while (edited) {
+        edited = false;
+        groups.forEach(group => {
+            let newSteps = replaceOpposite(steps, ...group);
+            edited = newSteps.edited || edited;
+            steps = newSteps.sortedSteps;
+        });
+    }
+
+    return steps.length;
+}
+
+console.log(eleven(input.split(',')));
