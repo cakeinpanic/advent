16c105a175e4e38770568f6e71237b3f04931b2a
cakeinpanic <cakeinpanic@gmail.com>
2017-12-18 01:39:16 +0300

    ad 15 extended day solution

--- 15.js
+++ 15.js
@@ -1,27 +1,52 @@
-function getNext(prev, factor, divider) {
-    let multiplied = (prev * factor);
-    return multiplied - Math.floor(multiplied / divider) * divider;
+const DIVIDER = 2147483647;
+const AFACTOR = 16807;
+const BFACTOR = 48271;
+
+function getNext(prev, factor) {
+    const multiplied = (prev * factor);
+    return multiplied - Math.floor(multiplied / DIVIDER) * DIVIDER;
 }
 
-function fifteen(inputA, inputB, totalPairs = 40000000) {
-    const aFactor = 16807;
-    const bFactor = 48271;
-    const divider = 2147483647;
+function generator(prev, factor, pickyDivider) {
+    let result = getNext(prev, factor);
 
+    if (pickyDivider) {
+        return result % pickyDivider
+            ? generator(result, factor, pickyDivider)
+            : result;
+    }
+}
+
+function generatorA(prev, isPicky) {
+    return generator(prev, AFACTOR, isPicky ? 4 : 0);
+}
+
+function generatorB(prev, isPicky) {
+    return generator(prev, BFACTOR, isPicky ? 8 : 0);
+}
+
+
+function fifteen(inputA, inputB, totalPairs = 40000000, isPicky = false) {
     let result = 0;
     const stabilizer = parseInt(new Array(16).fill(1).join(''), 2);
 
     for (let i = 0; i < totalPairs; i++) {
-        inputA = getNext(inputA, aFactor, divider);
-        inputB = getNext(inputB, bFactor, divider);
-        const xorA = inputA & stabilizer;
-        const xorB = inputB & stabilizer;
+        inputA = generatorA(inputA, AFACTOR, isPicky);
+        inputB = generatorB(inputB, BFACTOR, isPicky);
 
-        if (xorA === xorB) {
+        const shiftedA = inputA & stabilizer;
+        const shiftedB = inputB & stabilizer;
+
+        if (shiftedA === shiftedB) {
             result++;
         }
     }
     return result;
 }
 
-console.log(fifteen(512, 191));
+function fifteenExtended(inputA, inputB) {
+    return fifteen(inputA, inputB, 5000000, true);
+
+}
+
+console.log(fifteenExtended(512, 191));
