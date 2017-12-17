c69e26722e33f163dd092ecb62f0993b1aae8ad4
cakeinpanic <cakeinpanic@gmail.com>
2017-12-17 15:05:52 +0300

    add fourteen extended day solution

--- 14.js
+++ 14.js
@@ -1,19 +1,20 @@
 const {makeKnotHash} = require('./10');
 const {getFilledArray} = require('./utlis');
 
+
+const input = 'ffayrhll';
+
 function getLine(input, onlyUsed) {
-    let t = makeKnotHash(input)
+    return makeKnotHash(input)
         .split('')
         .map((letter) => parseInt(letter, 16).toString(2))
         .map((binary) => {
             if (!onlyUsed) {
                 return new Array(4 - binary.length).fill(0).join('') + binary;
             }
-            return binary.split('').filter(n => n==='1').join('')
+            return binary.split('').filter(n => n === '1').join('')
         })
         .join('');
-
-    return t
 }
 
 function fourteen(input, base = 128) {
@@ -24,4 +25,35 @@ function fourteen(input, base = 128) {
     return table.length;
 }
 
-console.log(fourteen('ffayrhll'))
+
+function fourteenExtended(input, base = 128) {
+    let table = getFilledArray(base).map((el, i) => `${input}-${i}`)
+                                    .map(string => getLine(string).split(''));
+
+    // todo move to utils and use in day 3
+    function getElement(y, x) {
+        return table[y] && table[y][x] || 0;
+    }
+
+    function findNeighbours(y, x) {
+        if (getElement(y, x) == 1) {
+            table[y][x] = 0;
+            findNeighbours(y - 1, x);
+            findNeighbours(y + 1, x);
+            findNeighbours(y, x - 1);
+            findNeighbours(y, x + 1);
+            return 1;
+        }
+        return 0;
+    }
+
+    let numberOfGroups = 0;
+    for (let i = 0; i < base; i++) {
+        for (let j = 0; j < base; j++) {
+            numberOfGroups += findNeighbours(i, j);
+        }
+    }
+    return numberOfGroups;
+}
+
+console.log(fourteenExtended(input));
