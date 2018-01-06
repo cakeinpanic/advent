49dd9363a5c3453d44620e306ab9a5e7fa8be13e
cakeinpanic <cakeinpanic@gmail.com>
2018-01-06 03:22:49 +0300

    add 22 day extended solution

--- 22.js
+++ 22.js
@@ -1,21 +1,38 @@
 let input = require('./data/22');
-let {getFilledArray, getTableElement, getNextElementCoordinates, turnLeft, turnRight, UP} = require('./utlis');
+let {getFilledArray, getTableElement, getOppositeDirection, getNextElementCoordinates, turnLeft, turnRight, UP} = require('./utlis');
 
 function prepareInput(input) {
     return input.split('\n').map(t => t.split(''));
 }
 
+const CLEAN = 'clean';
+const WEAKENED = 'weakened';
+const INFECTED = 'infected';
+const FLAGGED = 'flagged';
+
 class Cell {
-    constructor(value) {
-        this.infected = value === '#';
+    constructor(value, extended) {
+        this.status = value === '#' ? INFECTED : CLEAN;
+        this.flags = extended ? [CLEAN, WEAKENED, INFECTED, FLAGGED] : [CLEAN, INFECTED];
+    }
+
+    increaseFlag() {
+        const currentFlagIndex = this.flags.indexOf(this.status);
+        this.status = this.flags[(currentFlagIndex + 1) % this.flags.length];
+    }
+
+    get infected() {
+        return this.status === INFECTED;
     }
 }
 
 class Map {
-    constructor(input) {
+    constructor(input, extended) {
+        this.extended = extended;
         this.cells = prepareInput(input)
-            .map((line) => line.map((symbol) => new Cell(symbol)));
+            .map((line) => line.map((symbol) => new Cell(symbol, this.extended)));
 
+        this.getCell = getTableElement.bind({table: this.cells});
         this.infectedMoves = 0;
         this.width = this.cells[0].length;
         this.height = this.cells.length;
@@ -26,17 +43,28 @@ class Map {
 
         this.x = enter.x;
         this.y = enter.y;
-
-
     };
+
+    changeDirection() {
+        if (this.extended) {
+            if (this.currentCell.status === WEAKENED) {
+                return;
+            }
+            if (this.currentCell.status === FLAGGED) {
+                this.direction = getOppositeDirection(this.direction);
+                return;
+            }
+        }
+        this.direction = this.currentCell.infected ? turnRight(this.direction) : turnLeft(this.direction);
+    }
 
     findEnter() {
         return {x: Math.floor(this.width / 2), y: Math.floor(this.height / 2)};
     }
 
     makeMove() {
-        this.direction = this.currentCell.infected ? turnRight(this.direction) : turnLeft(this.direction);
-        this.currentCell.infected = !this.currentCell.infected;
+        this.changeDirection();
+        this.currentCell.increaseFlag();
 
         if (this.currentCell.infected) {
             this.infectedMoves++;
@@ -45,11 +73,11 @@ class Map {
         const {x, y} = getNextElementCoordinates(this.x, this.y, this.direction);
         this.x = x;
         this.y = y;
-        let newCell = getTableElement.call({table: this.cells}, this.y, this.x);
+        let newCell = this.getCell(this.y, this.x);
 
         if (!newCell) {
             this.expandMap();
-            newCell = getTableElement.call({table: this.cells}, this.y, this.x);
+            newCell = this.getCell(this.y, this.x);
         }
 
         this.currentCell = newCell;
@@ -58,31 +86,31 @@ class Map {
     expandMap() {
         this.width += 2;
         this.height += 2;
-        this.cells.forEach(line => line.push(new Cell('.')));
-        this.cells.forEach(line => line.unshift(new Cell('.')));
+        this.cells.forEach(line => line.push(new Cell('.', this.extended)));
+        this.cells.forEach(line => line.unshift(new Cell('.', this.extended)));
 
-        this.cells.push(getFilledArray(this.width).map(() => new Cell('.')));
-        this.cells.unshift(getFilledArray(this.width).map(() => new Cell('.')));
+        this.cells.push(getFilledArray(this.width).map(() => new Cell('.', this.extended)));
+        this.cells.unshift(getFilledArray(this.width).map(() => new Cell('.', this.extended)));
 
         this.x++;
         this.y++;
     }
-
-    log() {
-        console.log(this.cells.map(line => line.map(el => el.infected ? '#' : '.').join('')).join('\n'))
-    }
 }
 
 
-function nineteen(input) {
-    const maze = new Map(input);
+function twentyTwo(input, moves = 10000, extended) {
+    const maze = new Map(input, extended);
     let i = 0;
-    while (i++ < 10000) {
+    while (i++ < moves) {
         maze.makeMove()
     }
 
     return maze.infectedMoves;
 }
 
+function twentyTwoExtended(input) {
+    return twentyTwo(input, 10000000, true)
+}
 
-console.log(nineteen(input));
+console.log(twentyTwo(input));
+console.log(twentyTwoExtended(input));
